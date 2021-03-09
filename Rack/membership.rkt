e725af15a32b832ca088d4c5ac84913db96cb5cc
Sam Trenter <sntrenter@gmail.com>
2021-03-09 10:48:49 -0600

    membership.rkt

---
+++ Rack/membership.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(define (membership atm a_list)
+  (cond
+       ((null? a_list) #f)
+       ((eq? atm (car a_list)) #t)
+       (else (membership atm (cdr a_list)))
+ ))
+
+(membership 'a '(d c e b))
+
