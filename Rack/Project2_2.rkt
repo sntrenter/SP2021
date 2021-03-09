f208f07498f4f3adaa5b5aad166c1ff7f454f274
Sam Trenter <sntrenter@gmail.com>
2021-03-09 01:45:32 -0600

    something something malice

--- Rack/Project2_2.rkt
+++ Rack/Project2_2.rkt
@@ -8,16 +8,30 @@
 ;not  have two elements, the function should return an empty list.
 
 
+
 (define (rem_second l)
-(cons (car l) (cddr l))
+    (cond 
+            ((null? l) '())
+            ((null? (cdr l)) '() )
+            (else (cons (car l) (cddr l)))
+    )
 )
 
-(define l1 (list 1 2 3 4 5 6))
+
+
+
+(define l1 '(1 2 3 4 5 6))
 (define l2 (list 1 (list 2 3 4) 3 4 5 6))
+(define l3 (list 1 ))
+(define l4 '())
 l1
 (rem_second l1)
 l2
 (rem_second l2)
+l3
+(rem_second l3)
+l4
+(rem_second l4)
 
 
 
